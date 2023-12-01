6a07db48da5de586566ed0d79588c815ab0c598e
Thang, Dang Hieu <dhthang1998@gmail.com>
2023-12-01 09:58:24 +0700

    Migrate to .NET 8

---
+++ TodoApp.Product.API.Test/BaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApp.Infrastructure.Core.Extensions;
+using TodoApp.Product.API.Infrastructure;
+
+namespace TodoApp.Product.API.Test;
+
+public abstract class BaseTest
+{
+    protected IServiceProvider ServiceProvider { get; private set; }
+
+    protected void SetUp(string configurationPath = "settings.json")
+    {
+        IConfigurationRoot? configuration = new ConfigurationBuilder()
+            .AddJsonFile(configurationPath)
+            .Build();
+        ServiceCollection serviceCollection = new();
+        serviceCollection.AddUnitOfWork<ProductDbContext>();
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    protected List<MasterData> InitializeMasterData()
+    {
+        return 
+    }
+}
---
+++ TodoApp.Product.API.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TodoApp.Product.API.Test;
+
+public class Tests
+{
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
